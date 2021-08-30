dc676e16115449d38ff938cd2d17f09c9901b3da
Jose Cortes <josecortes@datawire.io>
2021-08-30 14:16:28 -0400

    Bring in the new demo-node from getambassador.io
    Signed-off-by: Jose Cortes <josecortes@datawire.io>

---
+++ src/ambassador-cloud-demo/components/NodeQuickStart/InterceptsLink.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+
+import { useAppState } from '../../context'
+import { getBaseUIUrl } from '../../utils/getBaseUrl';
+
+const InterceptsLink = ({ children }) => {
+  const { userServices, demoClusterMetadata } = useAppState();
+  const [previewUrl, setPreviewUrl] = useState('');
+
+  useEffect(() => {
+    if (userServices?.length && demoClusterMetadata?.name)
+      setPreviewUrl(userServices?.find(
+        (service) => (service?.cluster?.name === demoClusterMetadata?.name) && service.intercepts && service.intercepts[0]?.name === 'web',
+      )?.intercepts[0].previewURL || '');
+  }, [demoClusterMetadata, userServices]);
+
+  if (!previewUrl) return children;
+
+  return (
+    <a
+      href={`${getBaseUIUrl()}/services/web-svc/intercepts?env=Development&utm_source=docs&utm_medium=telepresence-quick-start-20210730&utm_content=completion`}
+      target="_blank"
+      rel="noreferrer"
+    >
+      {children}
+    </a>
+  );
+};
+
+export { InterceptsLink }

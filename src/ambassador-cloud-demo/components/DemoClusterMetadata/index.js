f5355d0ab8489042445ad4d8260a8f5689008aa7
Jose Cortes <josecortes@datawire.io>
2021-09-02 18:25:12 -0400

    Revert "Bring in the new demo-node from getambassador.io"
    This reverts commit dc676e16115449d38ff938cd2d17f09c9901b3da.
    
    Signed-off-by: Jose Cortes <josecortes@datawire.io>

--- src/ambassador-cloud-demo/components/DemoClusterMetadata/index.js
+++
@@ -1,2 +0,0 @@
-export { DemoClusterMetadata } from './DemoClusterMetadata';
-export { ExpirationDate } from './ExpirationDate';
--- src/ambassador-cloud-demo/utils/demoCluster.js
+++
@@ -1,2 +0,0 @@
-export const demoClusterId = 'telepresence-demo';
-export const noDemoClusterFound = 'no demo cluster found associated to this account';

563348c3916e8d9d9a98773bfa2de4770e4df25d
Jose Cortes <josecortes@datawire.io>
2021-10-04 14:44:23 -0400

    Drop kubernaut link as it doesn't seem to be alive anymore.
    Signed-off-by: Jose Cortes <josecortes@datawire.io>

--- src/pages/related-projects.js
+++ src/pages/related-projects.js
@@ -14,8 +14,6 @@ Ambassador Labs has a number of open source projects that are designed to improv
 * [Forge](https://forge.sh) allows developers to define and deploy multi-container applications into Kubernetes, from source, incredibly fast.
 
 * [Ambassador](https://www.getambassador.io) is a Kubernetes-native API Gateway built on the [Envoy Proxy](https://envoyproxy.github.io), designed for microservices.
-
-* [Kubernaut](https://kubernaut.io) provides instant, ephemeral Kubernetes clusters for use in CI systems.
 `}</Markdown>
     </EasyLayout>
   );

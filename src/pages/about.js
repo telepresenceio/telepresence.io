fe96c916d898be99bcb1902b1df2574e1c223659
aaronUnosquare <aaron.sosa@unosquare.com>
2022-09-21 16:06:30 -0500

    Redesign of About page
    Signed-off-by: aaronUnosquare <aaron.sosa@unosquare.com>

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,42 +3,58 @@ import HubspotForm from 'react-hubspot-form';
 
 import EasyLayout from '../components/EasyLayout';
 import Markdown from '../components/Markdown';
+import TelepresenceIcon from '../assets/images/telepresence-edgy.svg';
 
 export default function AboutPage({ location }) {
-  return (
-    <EasyLayout title="About" location={location}
-                description="Telepresence: a local development environment for a remote Kubernetes cluster">
-      <section id="about-page" className="bg-white">
-        <div className="container">
-          <h1 className="text-lg">About</h1>
-          <Markdown>{`
-
-Telepresence is an open source tool that lets you run a single service locally, while connecting that service to a
-remote Kubernetes cluster.
-
-Telepresence was originally built by the team at [Ambassador Labs](https://www.getambassador.io/), which builds open
-source development tools for Kubernetes, including [Forge](https://forge.sh/) and
-[Ambassador](https://www.getambassador.io).  The current list of maintainers of Telepresence are listed in the
-[MAINTAINERS.md](https://github.com/telepresenceio/telepresence/blob/master/MAINTAINERS.md) file.
-
-We welcome all community contributions.  If you find a bug or a mistake in the documentation, you can help us out by
-[submitting an issue](https://github.com/telepresenceio/telepresence/issues/new) or a pull request with a fix.  If you
-have questions, join our active [Slack community](https://a8r.io/slack) or [contact
-us](https://www.getambassador.io/editions/) about paid support plans.
-
-`}</Markdown>
-          <div className="bg-white content-box text-center">
-            <h1 className="text-lg">Stay Updated</h1>
-            <p className="font-light text-md">Telepresence is under active development. Subscribe to get updates and announcements:</p>
-            <div className="mailing-list-signup">
-              <HubspotForm
-                portalId="485087"
-                formId="956287a4-7614-486b-91bd-28c9a91949cb"
-              />
-            </div>
-          </div>
-        </div>
-      </section>
-    </EasyLayout>
-  );
+	return (
+		<EasyLayout
+			title="About"
+			location={location}
+			description="Telepresence: a local development environment for a remote Kubernetes cluster"
+		>
+			<section className="aboutPageContainer">
+				<div className="aboutPageContainer-item">
+					<h1>About</h1>
+					<p>
+						Telepresence is an open source tool that lets you run a single
+						service locally, while connecting that service to a remote
+						Kubernetes cluster.
+					</p>
+					<p>
+						Telepresence was originally built by the team at Ambassador Labs,
+						which builds open source development tools for Kubernetes, including
+						Forge and Ambassador. The current list of maintainers of
+						Telepresence are listed in the MAINTAINERS.md file.
+					</p>
+					<p>
+						We welcome all community contributions. If you find a bug or a
+						mistake in the documentation, you can help us out by submitting an
+						issue or a pull request with a fix. If you have questions, join our
+						active Slack community or contact us about paid support plans.
+					</p>
+				</div>
+				<div className="aboutPageContainer-item">
+					<img
+						src={TelepresenceIcon}
+						alt="Telepresence Edgy icon"
+						width="300"
+						height="300"
+					/>
+				</div>
+			</section>
+			<section className="aboutPageStayUpdated">
+				<h4>Stay Updated</h4>
+				<p>
+					Telepresence is under active development. Subscribe to get updates and
+					announcements:
+				</p>
+				<div className="aboutPageHubspotForm">
+					<HubspotForm
+						portalId="485087"
+						formId="956287a4-7614-486b-91bd-28c9a91949cb"
+					/>
+				</div>
+			</section>
+		</EasyLayout>
+	);
 }

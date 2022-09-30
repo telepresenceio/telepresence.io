d31e8628a0de58df8420d6584863f0df03ea14f0
aaronUnosquare <aaron.sosa@unosquare.com>
2022-09-30 13:24:36 -0500

    Add new main menu design
    Signed-off-by: aaronUnosquare <aaron.sosa@unosquare.com>

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classnames from 'classnames';
 import { Helmet } from 'react-helmet';
 
 import Platform from '@src/components/Platform';
+import MainNav from '@src/components/MainNav';
 
 import './globalHelpers.less';
 import './layout.less';
 import './docs-layout.less';
 import './home.css';
 
-import ImgLogo from '../../assets/images/telepresence-logo.png';
+import TelepresenceLogo from '../../assets/images/telepresence-edgy.svg';
 
 export default function Layout({ location, children }) {
+	const [open, setOpen] = useState(false);
+
+	const toggleNav = () => setOpen(!open);
+
 	return (
 		<Platform.Provider>
 			<Helmet>
@@ -21,53 +27,54 @@ export default function Layout({ location, children }) {
 				<link rel="shortcut icon" href="/favicon.ico" />
 			</Helmet>
 
-			<header className="white-bg">
-				<div className="navigation-left">
-					<a className="datawire-link" href="/">
-						<img alt="Telepresence" src={ImgLogo} />
-					</a>
-					<ul className="main-navigation">
-						<li>
-							<a href="/docs/latest/">Docs</a>
-						</li>
-						<li>
-							<a href="/case-studies">Case Studies</a>
-						</li>
-						<li>
-							<a href="/community">Community</a>
-						</li>
-						<li>
-							<a href="/about">About</a>
-						</li>
-						<li>
-							<a
-								target="_blank"
-								rel="noreferrer"
-								href="https://github.com/telepresenceio/telepresence"
-							>
-								GitHub
-							</a>
-						</li>
-					</ul>
+			<header className="header">
+				<div className="headerContainer">
+					<div className="MobileTop">
+						<a href="/" className="LogoLink">
+							<img
+								src={TelepresenceLogo}
+								alt="Telepresence Logo"
+								width="32"
+								height="32"
+							/>
+							<p>Telepresence</p>
+						</a>
+						<button
+							onClick={toggleNav}
+							className={classnames('Burger', { open: open })}
+							aria-label="Open and close menu"
+						>
+							<span />
+							<span />
+							<span />
+							<span />
+						</button>
+					</div>
+					<nav className="desktopNav">
+						<MainNav />
+						<a
+							className={classnames('btn-purple-light')}
+							target="_blank"
+							rel="noreferrer"
+							href="https://a8r.io/slack"
+						>
+							need help?
+						</a>
+					</nav>
 				</div>
-				<div className="navigation-right">
-					<ul className="main-navigation right">
-						<li>
-							<div className="dropdown">
-								<button className="dropbtn">▾ Need Help?</button>
-								<div className="dropdown-content">
-									<a
-										target="_blank"
-										rel="noreferrer"
-										href="https://a8r.io/slack"
-									>
-										Community Slack
-									</a>
-								</div>
-							</div>
-						</li>
-					</ul>
-				</div>
+				{open && (
+					<nav className="nav">
+						<MainNav />
+						<a
+							className={classnames('btn-purple-light', 'needHelpButton')}
+							target="_blank"
+							rel="noreferrer"
+							href="https://a8r.io/slack"
+						>
+							need help?
+						</a>
+					</nav>
+				)}
 			</header>
 
 			<div className="main-body">{children}</div>
---
+++ src/components/MainNav/MainNav.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import '../Layout/layout.less';
+
+const LINKS = [
+	{ label: 'Quick Start', link: '/docs/latest/quick-start/' },
+	{ label: 'Docs', link: '/docs/latest/' },
+	{ label: 'Case Studies', link: '/case-studies' },
+	{ label: 'Community', link: '/community' },
+	{ label: 'About', link: '/about' },
+	{
+		label: 'Github',
+		link: 'https://github.com/telepresenceio/telepresence',
+		isExternal: true,
+	},
+];
+
+const MainNav = () => {
+	return (
+		<ul>
+			{LINKS.map((item, index) => {
+				const linkProps = {
+					href: item.link,
+				};
+
+				if (item.isExternal) {
+					linkProps.target = '_blank';
+					linkProps.rel = 'noreferrer';
+				}
+
+				return (
+					<li key={index}>
+						<a {...linkProps}>{item.label}</a>
+					</li>
+				);
+			})}
+		</ul>
+	);
+};
+
+export default MainNav;

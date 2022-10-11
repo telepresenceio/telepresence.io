3f29b5759b25ea1f2b24ea7a736b05403ea40737
Leonel Contreras <lcontreras@datawire.io>
2022-10-11 15:56:42 -0500

    resolve conflicts
    Signed-off-by: Leonel Contreras <lcontreras@datawire.io>

--- src/components/MainNav/MainNav.js
+++ src/components/MainNav/MainNav.js
@@ -3,7 +3,7 @@ import '../Layout/layout.less';
 
 const LINKS = [
 	{ label: 'Quick Start', link: '/docs/latest/quick-start/' },
-	{ label: 'Docs', link: 'https://www.getambassador.io/docs/telepresence/' },
+	{ label: 'Docs', link: '/docs/latest/' },
 	{ label: 'Case Studies', link: '/case-studies' },
 	{ label: 'Community', link: '/community' },
 	{ label: 'About', link: '/about' },

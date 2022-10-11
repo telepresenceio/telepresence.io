40f35ffd3185556b02a43a54496991bb7c7168be
Leonel Contreras <lcontreras@datawire.io>
2022-10-11 16:15:46 -0500

    rebase
    Signed-off-by: Leonel Contreras <lcontreras@datawire.io>

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -12,6 +12,7 @@ import './layout.less';
 import './docs-layout.less';
 import './home.css';
 
+<<<<<<< HEAD
 import TelepresenceLogo from '../../assets/images/telepresence-edgy.svg';
 
 export default function Layout({ location, children }) {
@@ -83,3 +84,168 @@ export default function Layout({ location, children }) {
 		</Platform.Provider>
 	);
 }
+=======
+import ImgLogo from '../../assets/images/telepresence-logo.png';
+
+export default function Layout({ location, children }) {
+	return (
+		<Platform.Provider>
+			<Helmet>
+				<meta
+					name="keywords"
+					content="Telepresence, Kubernetes, microservices"
+				/>
+				<link rel="shortcut icon" href="/favicon.ico" />
+			</Helmet>
+
+<<<<<<< HEAD
+      <header className="white-bg">
+        <div className="navigation-left">
+          <a className="datawire-link" href="/">
+            <img alt="Telepresence" src={ImgLogo}/>
+          </a>
+          <ul className="main-navigation">
+            <li>
+              <a href="https://www.getambassador.io/docs/telepresence/">Docs</a>
+            </li>
+            <li>
+              <Link to="/docs/latest/quick-start/">Quick start</Link>
+            </li>
+            <li>
+              <Link to="/case-studies">Case Studies</Link>
+            </li>
+            <li>
+              <Link to="/community">Community</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <a target="_blank" rel="noreferrer" href="https://github.com/telepresenceio/telepresence">GitHub</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navigation-right">
+          <ul className="main-navigation right">
+            <li>
+              <div className="dropdown">
+                <button className="dropbtn">▾ Need Help?</button>
+                <div className="dropdown-content">
+                  <a target="_blank" rel="noreferrer" href="https://a8r.io/slack">Community Slack</a>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </header>
+=======
+			<header className="white-bg">
+				<div className="navigation-left">
+					<a className="datawire-link" href="/">
+						<img alt="Telepresence" src={ImgLogo} />
+					</a>
+					<ul className="main-navigation">
+						<li>
+							<a href="/docs/latest/">Docs</a>
+						</li>
+						<li>
+							<a href="/case-studies">Case Studies</a>
+						</li>
+						<li>
+							<a href="/community">Community</a>
+						</li>
+						<li>
+							<a href="/about">About</a>
+						</li>
+						<li>
+							<a
+								target="_blank"
+								rel="noreferrer"
+								href="https://github.com/telepresenceio/telepresence"
+							>
+								GitHub
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div className="navigation-right">
+					<ul className="main-navigation right">
+						<li>
+							<div className="dropdown">
+								<button className="dropbtn">▾ Need Help?</button>
+								<div className="dropdown-content">
+									<a
+										target="_blank"
+										rel="noreferrer"
+										href="https://a8r.io/slack"
+									>
+										Community Slack
+									</a>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</header>
+>>>>>>> 6651e0a (Remove banner)
+
+			<div className="main-body">{children}</div>
+
+<<<<<<< HEAD
+      <div className="main-body">{children}</div>
+
+      <footer className="white-bg">
+        <ul className="main-navigation flex-center">
+          <li>
+            <a href="https://www.getambassador.io/docs/telepresence/">Docs</a>
+          </li>
+          <li>
+            <Link to="/case-studies">Case Studies</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <a target="_blank" rel="noreferrer" href="https://github.com/telepresenceio/telepresence">GitHub</a>
+          </li>
+          <li>
+            <a target="_blank" rel="noreferrer" href="https://a8r.io/slack">Slack</a>
+          </li>
+        </ul>
+      </footer>
+    </Platform.Provider>
+  );
+};
+=======
+			<footer className="white-bg">
+				<ul className="main-navigation flex-center">
+					<li>
+						<a href="/docs/latest">Docs</a>
+					</li>
+					<li>
+						<a href="/case-studies">Case Studies</a>
+					</li>
+					<li>
+						<a href="/about">About</a>
+					</li>
+					<li>
+						<a
+							target="_blank"
+							rel="noreferrer"
+							href="https://github.com/telepresenceio/telepresence"
+						>
+							GitHub
+						</a>
+					</li>
+					<li>
+						<a target="_blank" rel="noreferrer" href="https://a8r.io/slack">
+							Slack
+						</a>
+					</li>
+				</ul>
+			</footer>
+		</Platform.Provider>
+	);
+}
+>>>>>>> 6651e0a (Remove banner)
+>>>>>>> 83dbe4c (rebase)

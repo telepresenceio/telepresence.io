40c58cf35fe1d1a4b8208f487476aa513f43c2c2
Your Name <youremail@example.com>
2025-04-24 16:40:25 +0200

    testing gpgsign //SEO changes like texts on index page & added robots.txt and schema.org
    Signed-off-by: Your Name <mattias@tada.se>
    Signed-off-by: markrity <markrity@gmail.com>

--- src/theme/Root/index.tsx
+++ src/theme/Root/index.tsx
@@ -1,13 +1,38 @@
-import React, {JSX} from 'react';
-import {ThemeProvider} from "@mui/material/styles";
-import {customTheme} from "./custom-themes";
+import React, { JSX } from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import { customTheme } from './custom-themes';
+import Head from '@docusaurus/Head';
 
-function Root({children}): JSX.Element {
-	return (
-		<ThemeProvider theme={customTheme()}>
-			{children}
-		</ThemeProvider>
-	);
+function Root({ children }: { children: JSX.Element }): JSX.Element {
+  return (
+    <ThemeProvider theme={customTheme()}>
+      {/* Add the JSON-LD Structured Data in the <head> */}
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+				"@context": "https://schema.org",
+				"@type": "WebSite",
+				"url": "https://telepresence.io",
+				"name": "Telepresence.io",
+				"description": "Telepresence is a platform that allows developers to run their local code in a remote Kubernetes cluster for easier testing and debugging.",
+				"publisher": {
+				  "@type": "Organization",
+				  "name": "Telepresence"
+				},
+				"mainEntityOfPage": {
+				  "@type": "WebPage",
+				  "@id": "https://telepresence.io"
+				},
+			  }
+			  )
+          }}
+        />
+      </Head>
+      {children}
+    </ThemeProvider>
+  );
 }
 
-export default Root
+export default Root;
